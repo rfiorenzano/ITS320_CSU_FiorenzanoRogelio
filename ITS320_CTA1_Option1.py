aedf81690f8ba282a8d910ed58cb4decfae0f795
Rogelio Fiorenzano <rogelio.fiorenzano+github@gmail.com>
2020-10-09 11:59:10 +0800

    Added ITS320_CTA1_Option1.py file
    This was ITS320: Basic Programming Module 01 Critical Thinking exercise for Python coding. This submission was a basic program which drew a small rat utilizing special characters to the console. In this case, additional variables were included as well for fun.

---
+++ ITS320_CTA1_Option1.py
@@ -0,0 +1,10 @@
+print("\
+          (\-.  \n \
+         /  _`> \n \
+ _)     / _)=   \n \
+(      / _/     \n \
+ `-.__(___)_.     ")
+
+extra_credit = str("2020 is the Year of the Rat in the Chinese Zodiac.")
+gratitude = str("Thank you professor for not making the first coding submission excrutiating!")
+print("\n" + "\n" + extra_credit + "\n" + "\n" + gratitude)
