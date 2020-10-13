483d061a472fd4ad8c3a8a9fa434efc94e0d4ec9
Rogelio Fiorenzano <rogelio.fiorenzano@gmail.com>
2020-10-13 16:37:17 +0800

    New assignment
    Uploading new class assignment

---
+++ ITS320_CTA2_Option1.py
@@ -0,0 +1,10 @@
+# python3 (3.6)
+# Coded on iPad Pro 2020 4th Generation
+# Pythonista an Apple iPad App
+# MIT License Copyright (c) 2020 Rogelio Fiorenzano
+#
+# ITS320: Basic Programming
+# Colorado State University Global
+# Dr. Joe Turano
+
+print()
