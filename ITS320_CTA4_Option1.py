f33a65b8791d0b42e5e64646dd4802e7fe734b00
Rogelio Fiorenzano <rogelio.fiorenzano@gmail.com>
2020-10-29 16:31:06 +0800

    Begin Module 4 CTA Assignment
    Upload template framework for assignment

---
+++ ITS320_CTA4_Option1.py
@@ -0,0 +1,21 @@
+# python3 (3.6)
+# Coded on iPad Pro 2020 4th Generation
+# Pythonista an Apple iPad App
+# MIT License Copyright (c) 2020 Rogelio Fiorenzano
+#
+# ITS320: Basic Programming
+# Colorado State University Global
+# Dr. Joe Option #1: Repetition Control Structure - Five Floating Point Numbers
+
+# Assignment Instructions
+# Write a program that utilizes a loop to read a set of five floating-point values from user input. Ask the user to enter the values, then print the following data:
+
+# Total
+# Average
+# Maximum
+# Minimum
+# Interest at 20% for each original value entered by the user.
+# Use the formula: Interest_Value = Original_value + Original_value*0.2
+
+# Assigning CTA Variables for Review
+
