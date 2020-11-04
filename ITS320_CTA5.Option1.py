4dd354eba0a03671d571ffe7a3fef2058ae233df
Rogelio Fiorenzano <rogelio.fiorenzano+github@gmail.com>
2020-11-04 14:40:16 +0800

    Create ITS320_CTA5.Option1.py
    Beginning of assignment for module 5

---
+++ ITS320_CTA5.Option1.py
@@ -0,0 +1,17 @@
+# python3 (3.6)
+# Coded on iPad Pro 2020 4th Generation
+# Pythonista an Apple iPad App
+# MIT License Copyright (c) 2020 Rogelio Fiorenzano
+#
+# ITS320: Basic Programming
+# Colorado State University Global
+# Dr. Joseph Turano
+
+# Option #1: String Values in Reverse Order
+
+# Assignment Instructions
+
+# Write a Python function that will accept as input three string values from a user. The method will return to the user a concatenation of the string values in reverse order. The function is to be called from the main method.
+# In the main method, prompt the user for the three strings.
+
+
