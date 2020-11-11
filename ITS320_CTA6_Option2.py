e5a19a0521c475adbc69861da0b097b7a2749370
Rogelio Fiorenzano <rogelio.fiorenzano+github@gmail.com>
2020-11-11 15:17:41 +0800

    Begin Module 6 Assignment
    Creating template file for coding on iPad

---
+++ ITS320_CTA6_Option2.py
@@ -0,0 +1,22 @@
+# python3 (3.6)
+# Coded on iPad Pro 2020 4th Generation
+# Pythonista an Apple iPad App
+# MIT License Copyright (c) 2020 Rogelio Fiorenzano
+#
+# ITS320: Basic Programming
+# Colorado State University Global
+# Dr. Joseph Turano
+
+# Option #1: String Values in Reverse Order
+
+# Assignment Instructions
+
+# Compute the Cartesian product of two lists AxB where each list has no more than 10 numbers
+
+# For example, if the user supplies the two input lists:
+#    A = [1,2]
+#    B = [3,4]
+
+#    then the Cartesian product output should be:
+#    AxB = [(1,3),(1,4),(2,3),(2,4)]
+
